6163cb3499263e056fd08b2876c741ad097a2184
Aashik Shihaab <sihaabama@gmail.com>
2024-03-13 00:58:55 +0530

    adding dice challenge

---
+++ Dice Challenge/index.js
@@ -0,0 +1,28 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+function changeDiceImg1() {
+    document
+        .querySelector(".img1")
+        .setAttribute("src", `./images/dice${randomNumber1}.png`);
+}
+
+function changeDiceImg2() {
+    document
+        .querySelector(".img2")
+        .setAttribute("src", `./images/dice${randomNumber2}.png`);
+}
+
+function changeTitle() {
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = " 🚩 Player 1 Wins";
+    } else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
+    } else {
+        document.querySelector("h1").innerHTML = "Draw!";
+    }
+}
+
+changeDiceImg1();
+changeDiceImg2();
+changeTitle();
